4c006db907f78e0d06e193b46dbb763af9d9dfcd
camrohlof <camrohlof@gmail.com>
2019-09-13 16:09:53 -0400

    inital

--- displays/admin.py
+++ displays/admin.py
@@ -4,6 +4,12 @@ from .models import Location, EventType, Event
 
 # Register your models here.
 
-admin.site.register(Event)
+#admin.site.register(Event)
 admin.site.register(Location)
 admin.site.register(EventType)
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    date_hierarchy = 'day'
+    list_display = ('name', 'day', 'time', 'location')
+    search_fields = ['name']
